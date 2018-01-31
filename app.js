0e76ed0f89ad16a910f9d079b0df0a05da5fd15e
christianbryant-rva <christianbryantmusic@gmail.com>
2018-01-31 04:03:09 -0800

    Adding basic Routes -cwb

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 var express = require('express')
 var app = express()
 
-// app.use(function(req, res, next) {
-//     console.log('hey')
-// })
 
-app.get('/', (req, res) => res.send('Hello Christian'))
+// respond with simple string when a GET req is made to homepage
 
+app.get('/about', function(req, res, next) {
+  if (req.path === '/about') {
+      res.send('ABOUT')
+  } else {
+    next()
+  }
+
+})
+
+app.get('/home', function(req, res, next) {
+    if (req.path === '/home') {
+      res.send('HOME')
+    } else {
+      next()
+    }
+})
+
+app.get('/resume', function(req, res, next) {
+    if (req.path === '/resume') {
+      res.send('RESUME')
+    } else {
+      next()
+    }
+})
+
+app.get('/contact', function(req, res, next) {
+    if (req.path === '/contact') {
+      res.send('CONTACT')
+    } else {
+      next()
+    }
+})
+
+
+
+app.use(function(req, res){
+  console.log('WooHoooooooo! EXPRESS ')
+})
 
 app.listen(3000, ()=> console.log("Christian's example App listening on port 3000!"))
