4ea506277b02458f303bfee0346114a0671df366
christianbryant-rva <christianbryantmusic@gmail.com>
2018-01-31 01:43:03 -0800

    First commit and push to master for new portfolio and RESTapi piece of christianbryant.com

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello'name))
+
+
+app.listen(3000, ()=> console.log("Christian's example App listening on port 3000!")) 
